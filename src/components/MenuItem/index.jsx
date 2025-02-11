e0bb6455575db3eed991ed6c7b2403124e8c5e1f
Meera Bhola <meerabhola3@gmail.com>
2025-02-11 18:52:09 -0500

    edited class

--- src/components/MenuItem/index.jsx
+++ src/components/MenuItem/index.jsx
@@ -2,12 +2,12 @@ import React from "react"
 
 const MenuItem = ({item }) => {
 return(
-       <div id={`menu-item-${item.itemId}`}>
+       <div className="menu-item" id={`menu-item-${item.itemId}`}>
           <p>${item.itemPrice}</p>
           <h1>{item.itemName}</h1>
           <p>{item.itemDescription}</p>
           <button>Add to Cart</button>
-          {/* uncomment the code below to display the ID */}
+          {/* uncomment the code below to visually display the ID */}
          {/* <p><em>ID: {`menu-item-${item.itemId}`}</em></p> */}
        </div>
 );
